76dfc84b5a248455a9182e29553950af8514714c
alamshahzad <alamshahzad115@gmail.com>
2026-04-03 17:52:24 +0530

    remove deposit code

--- create_account.c
+++ create_account.c
@@ -66,36 +66,6 @@ int Create_Account()
     
 }
 
-//Deposit;
-int Deposit()
-{
-    float amount;
-    
-
-    if(is_Account_Created==0)
-    {
-        printf(" Account Not Found! First Create Account.");
-        return 0;
-    }
-
-    printf("\n Enter Deposit Amount : ");
-    scanf("%f", &amount);
-
-    if(amount < 100)
-    {
-    printf("\n Enter a valid amount! Your Minimum Deposit Amount must be RS 100. ");
-    return 0;
-    }
-    else 
-    {
-        Balance += amount;
-        printf("\n Deposit Successful.");
-        printf("\n Your Current Balance : %f", Balance);
-        return 1;
-    }
-
-}
-
 int main()
 {
     Create_Account();
